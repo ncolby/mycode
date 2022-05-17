92051400709071d89e2a56f164fbb52ba37f4d20
nikkcold <ncolby@users.noreply.github.com>
2022-05-17 19:59:04 +0000

    join and split

---
+++ inputChallenge/challenge.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    name = input("Enter your name ")
+    day = input("Enter what day of the week it is... ")
+
+
+
+    print("Hello, " + name + "! Happy " + day + "!" )
+
+main()
