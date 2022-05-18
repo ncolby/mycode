4d567478d55fd37ac2d1cb50684ab856a0aca164
nikkcold <ncolby@users.noreply.github.com>
2022-05-18 16:29:02 +0000

    daily commit for update

---
+++ advlist/animal-list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def main():
+
+    animals = ["pig", "cow", "squirrel", "duck", "cat", "dog"]
+
+
+main()
---
+++ advlist/marvel.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+
+DoctorStrange = {"Real Name" : "Stephen Strange",
+        "Abilities" : ["Martial Arts", "Energy Blasts", "Sword Fighting"],
+        "Aliases" : ["Sherlock", "Sorcerer Supreme", "Vincent Stephens"],
+        "Universe" : "Marvel",
+        "Education" : ["MD", "Extensive Sorcery Training"]
+        }
+
+CaptainAmerica={'Skills and Abilities':['Master Tactician and Strategist',' Master Martial Artist',' Master Shield Fighter',' Advanced Military Operator',' Master Acrobat',' Indomitable Will',' Professional Sketch Artist',' Expert Marksman','Expert Swordsman','Weapons Proficiency',' Multilingual','Expert Vehicular Driver'],'Name':'steve Rogers','First Appearance':'Captain America Comics #1 (March, 1941)','creators':'Joe Simon, Jack Kirby','base of operations':'NYC'}
+
+
+
+
+print(f"Captain America is a super hero. First appeared in {CaptainAmerica['First Appearance']}. Was created by {CaptainAmerica['creators']}. His real name is {CaptainAmerica['Name']}. He primarily operates out of {CaptainAmerica['base of operations']}. But really, he saves the whole world, soooooo.")
+
+print(DoctorStrange)
